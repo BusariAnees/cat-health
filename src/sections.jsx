6aabb23cfadbdf02f8d788bac62625de6d3c1284
Anees <ayomideanees2003@icloud.com>
2025-05-30 21:55:40 +0100

    second section in motion

--- src/sections.jsx
+++ src/sections.jsx
@@ -2,10 +2,10 @@ export default function Sections (){
     return (
         <>
         <div className="section-1">
-            
+         cars are new here take them to the party 
         </div>
         <div className="section-1-1">
-
+when the tables turn always make sure you are on the right one
         </div>
         </>
     )
