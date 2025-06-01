37d39c68a35b6cebf1f206aa44f710c92d88569b
Anees <ayomideanees2003@icloud.com>
2025-06-01 11:04:01 +0100

    second section in progress

--- src/sections.jsx
+++ src/sections.jsx
@@ -2,10 +2,14 @@ export default function Sections (){
     return (
         <>
         <div className="section-1">
-         cars are new here take them to the party 
+      <h1 className="section-body-h1"> OUR APPROACH </h1> 
+      <p className="section-body-p">We’re helping dogs like yours live longer and stay healthier as they age.</p>
+<p className="section-body-p">Our products focus on preventive care, addressing the underlying causes of a range of age‑associated diseases to delay their onset and reduce their impact.</p>
+
         </div>
         <div className="section-1-1">
-when the tables turn always make sure you are on the right one
+        <p className="cat-name">Annie</p>
+<p className="cat-age"> Age 6</p>
         </div>
         </>
     )
