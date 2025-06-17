f9997e20f0cfceb60ba33a69a755e9449ebd5edb
Anees <ayomideanees2003@icloud.com>
2025-06-17 19:25:05 +0100

    built the pipeline

--- src/components/CatPipeline.jsx
+++ src/components/CatPipeline.jsx
@@ -1,10 +1,11 @@
-
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 const data = [
-  { name: "LOY-002", steps: [true, true, true, false], year: "2026" },
-  { name: "LOY-001", steps: [true, false, false, false], year: "2027" },
-  { name: "LOY-003", steps: [true, false, false, false], year: "2027" },
+  { name: "LOY-002", steps: [true, true, false], year: "2026" },
+  { name: "LOY-001", steps: [true, false, false], year: "2027" },
+  { name: "LOY-003", steps: [true, false, false], year: "2027" },
 ];
 
 const steps = [
@@ -17,12 +18,14 @@ const steps = [
 export default function CatPipeline() {
   return (
     <div className="bg-[#f8f7f2] pt-18 pb-12 px-40 font-sans">
-      <h2 className="text-4xl font-bold mb-0 text-left pb-8 !border-b-1 !border-black border-solid ">Pipeline overview</h2>
+      <h2 className="text-4xl font-bold mb-0 text-left pb-8 !border-b-1 !border-black border-solid ">
+        Pipeline overview
+      </h2>
       <div className="border-t border-gray-300 ">
         <div className="grid grid-cols-[100px_repeat(4,1fr)_80px] items-center ">
           <div></div>
           {steps.map((s, i) => (
-            <div key={i} className="text-sm font-medium text-gray-700 my-6">
+            <div key={i} className="text-sm font-bold text-gray-700 my-10 ">
               {s}
             </div>
           ))}
@@ -30,32 +33,41 @@ export default function CatPipeline() {
 
         {data.map((item, idx) => (
           <div
-            className="grid grid-cols-[100px_repeat(4,1fr)_80px] items-center mb-4"
+            className="grid grid-cols-[100px_repeat(4,0.5fr)_60px] items-center mb-4 "
             key={idx}
           >
-            <div className="font-semibold">{item.name}</div>
+            <div className="!border !border-black  rounded-[25px] p-[0.45rem] w-[5.5rem] text-[0.8rem] font-semibold ml-[0.8rem]">
+              {item.name}{" "}
+            </div>
+            <div className="h-[1px] flex-1 bg-black m-0"></div>
+
             {item.steps.map((done, stepIdx) => (
               <div key={stepIdx} className="flex items-center relative">
-                <div
-                  className={`w-4 h-4 rounded-full z-10 border-2 ${done ? "bg-black border-black" : "border-gray-300 bg-white"}`}
-                ></div>
-                {stepIdx < item.steps.length - 1 && (
-                  <div className="h-0.5 flex-1 bg-dotted ml-2"></div>
+                {done ? (
+  <div className="w-4 h-4 rounded-full bg-black border-2 border-black flex items-center justify-center text-[10px] text-white">
+    <FontAwesomeIcon icon={faCheck} />
+  </div>
+) : (
+  <div className="w-4 h-4 rounded-full !border-[1.3px] !border-[rgb(104,163,202)] flex items-center justify-center">
+    <div className="w-[0.45rem] h-[0.45rem] bg-[rgb(104,163,202)] rounded-full"></div>
+  </div>
+)}
+
+                {((idx===0 && stepIdx < 2) || (idx > 0 && stepIdx === 0 )) && (
+                  <div className="h-[1px] flex-1 bg-black m-0"></div>
                 )}
               </div>
             ))}
-            <div className="text-center text-sm bg-[#f5f2e8] px-2 py-1 rounded-full border border-gray-300">
+            <div className="text-center text-sm bg-none !border-[rgb(213,212,174)]  px-2 py-1 rounded-full !border">
               {item.year}*
             </div>
           </div>
         ))}
 
-        <p className="text-xs text-gray-500 mt-4">
+        <p className="text-xs text-gray-500  text-start mt-[2.5rem]">
           *FDA approval not guaranteed
         </p>
       </div>
     </div>
   );
 }
-
-  
